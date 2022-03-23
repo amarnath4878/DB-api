f9d27b7303af06c5b73dde21fd907bffe6af113c
amarnath4878 <amarnath4878@gmail.com>
2022-03-23 12:54:19 +0530

    database

--- api.go
+++ api.go
@@ -1,16 +1,66 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+
+	"pkg/database"
+
 	"github.com/gorilla/mux"
 )
-func main(){
+
+var dbConn *sql.DB
+
+type User struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Mobile    int64  `json:"mobileNumber"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+}
+
+func init() {
+	db := database.New("localhost", "root", "Shravan@123#", "8808", "gouser")
+	dbConn = db.Connect()
+	errPing := dbConn.Ping()
+	if errPing != nil {
+		log.Println(errPing)
+	}
+}
+
+func main() {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/",HomePageHandler)
-	http.ListenAndServe(":1119", router)
+	// Route handles & endpoints
+
+	router.HandleFunc("/", HomePageHandler).Methods(http.MethodGet)
+
+	// Create a movie
+	router.HandleFunc("/Signup", SignUpHandler).Methods(http.MethodPost)
+
+	// serve the app
+	fmt.Println("Server at 8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
-func HomePageHandler(w http.ResponseWriter, req *http.Request){
-    fmt.Fprint(w, "amarnath")
+
+func HomePageHandler(w http.ResponseWriter, r *http.Request) {
+	SearchKey := r.URL.Query().Get("q")
+	fmt.Fprint(w, SearchKey)
+}
+func SignUpHandler(w http.ResponseWriter, r *http.Request) {
+	var user *User
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	errJson := json.Unmarshal(body, &user)
+	if errJson != nil {
+		log.Fatalln(errJson)
+	}
+	w.WriteHeader(http.StatusAccepted)
+	fmt.Fprint(w, user)
 }
---
+++ pkg/database/database.go
@@ -0,0 +1,33 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type Database struct {
+	Address  string
+	Username string
+	Password string
+	Port     string
+	Database string
+}
+
+func New(address, username, pwd, port, database string) *Database {
+	return &Database{
+		Address:  address,
+		Username: username,
+		Password: pwd,
+		Port:     port,
+		Database: database,
+	}
+}
+func (db *Database) Connect() *sql.DB {
+	conn, err := sql.Open("mysql", "root:Shravan@123#@tcp(localhost:8808)/user")
+	if err != nil {
+		log.Println(err)
+	}
+	return conn
+}
